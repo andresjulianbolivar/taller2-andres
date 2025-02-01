502cfc864783f32b33549f420b3c35240f2496be
valeriacardenasf <v.cardenasf@uniandes.edu.co>
2025-02-01 17:56:57 -0500

    Generacion numeros aleatorios

--- taller2.py
+++ taller2.py
@@ -3,3 +3,11 @@
 #Modificación en la rama andres-branch
 
 import pandas as pd
+import numpy as np
+
+#Generar numeros aleatorios
+mu = 1
+sigma = 0.002
+n=50
+vals = np.random.normal(loc=mu, scale=sigma, size=n)
+print(vals)
